17637f3741ce0a8187a9d40570899cc3b32279e1
22021192-NguyenNgocTung <125429268+22021192-NguyenNgocTung@users.noreply.github.com>
2023-09-20 09:12:42 +0700

    3-Sum update

--- Three_Sum.java
+++ Three_Sum.java
@@ -12,18 +12,21 @@ public class Three_Sum {
                 }
             }
         }
-        for ( int i = 0 ; i < a.length ; ++i){
-            for ( int j = i + 1 ; j < a.length ; ++j){
-                int ans = - a[i] - a[j] ;
-                int low = 0 , high = a.length - 1 ;
-                int index = 0 ;
-                while( low <= high) {
-                    index = ( low + high) / 2 ;
-                    if ( ans < a[index] ) high = index - 1 ;
-                    else low = index + 1 ;
-                }
-                if (ans == a[index]){
-                    System.out.println(a[i] + " " + a[j] + " " + ans);
+        int dem = 0 ;
+        for ( int i = 0 ; i < a.length - 2 ; ++i){
+            int j = i + 1 ;
+            int sum = -100 ;
+            for ( int k = a.length - 1; k > j ; --k ){
+                while ( j < k ){
+                    sum = a[i] + a[j] + a[k] ;
+                    if ( sum == 0){
+                        dem ++ ;
+                        j++ ;
+                    }
+                    else if ( sum < 0 ){
+                        j ++ ;
+                    }
+                    else break ;
                 }
             }
         }
