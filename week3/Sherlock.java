8cf9108020fa039aece48ff4c9c95e69bb026bb9
22021192-NguyenNgocTung <125429268+22021192-NguyenNgocTung@users.noreply.github.com>
2023-12-11 13:03:22 +0700

    submit

--- week3/Sherlock.java
+++ week3/Sherlock.java
@@ -1,42 +1,68 @@
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class Sherlock {
-    public static boolean check(int[] a ){
+class Result {
+
+    /*
+     * Complete the 'balancedSums' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static String balancedSums(List<Integer> a) {
+        // Write your code here
         int sum = 0 ;
         for (int j : a) {
             sum += j;
         }
         int left_sum = 0 ;
-        for ( int i = 0 ; i < a.length ; ++i){
-            if ( i >= 1 ){
-                left_sum += a[i-1] ;
+        for ( int i = 0 ; i < a.size() ; ++i){
+            if (i >= 1) {
+                left_sum += a.get(i-1);
             }
-            System.out.println(left_sum) ;
-            if (sum - a[i] - left_sum == left_sum){
-                return true ;
+            if (sum - a.get(i) - left_sum == left_sum){
+                return "YES" ;
             }
         }
-        return false ;
+        return "NO" ;
     }
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in) ;
-        int n = input.nextInt();
-        boolean[] ans = new boolean[n] ;
-        for ( int i = 0 ; i < n ; ++i){
-            int size = input.nextInt() ;
-            int[] a = new int[size] ;
-            for ( int j = 0 ; j < size ; ++j){
-                a[j] = input.nextInt() ;
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                String result = Result.balancedSums(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
-            ans[i] = check(a) ;
-        }
-        for ( int i = 0 ; i < n ; ++i){
-            if (ans[i]) {
-                System.out.println("YES");
-            }
-            else {
-                System.out.println("NO");
-            }
-        }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
